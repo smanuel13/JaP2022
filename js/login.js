7608783f9a2a10fe071f2250d8cdb53dfeda5727
smanuel13 <110569128+smanuel13@users.noreply.github.com>
2022-08-31 12:05:58 -0300

    login finalc omentado

--- js/login.js
+++ js/login.js
@@ -1,11 +1,34 @@
+let correo = document.getElementById("email") 
+let contraseña = document.getElementById("password") 
+let botonRegistrar = document.getElementById("boton")
+/* se sacó el .value de las variables de arriba porque me traía undefined por eso no funcionaba el login
+console.log(correo);
+console.log(contraseña);
+console.log(botonRegistrar);
+ */ 
 
+
+/*este eventlistener es para que evite el default que es recargar la pagina original, si lo saco no funciona la redirección
+*/
+
+ botonRegistrar.addEventListener('click', (e) => {
+     e.preventDefault()
+ }
+
+ ) 
+/*defino la funcion loguear que tiene un condicional if para que si el campo correo y contraseña es distinto de vacío me redireccione a la portada.html*/ 
 function loguear() {
-    let correo = document.getElementById("email").value
-    let contraseña = document.getElementById("password").value
-    if ((correo !== "") && (contraseña !== "")){
-    window.location.href = ("portada.html")
+    /*console.log("sdsdsd")
+    console.log(correo.value)
+    console.log(contraseña.value)*/
+    if ((correo !== "" && contraseña !== ""))
+    {
+        window.location.href = "portada.html"
 } else{
     alert(("Ingrese un correo electronico y una contraseña"))
 }
-}
-document.getElementById("boton").addEventListener("click", loguear);
+
+  }
+ 
+  /*Al elemento boton le agrego un evento que es cuando haga click que corra la funcion loguear*/
+  botonRegistrar.addEventListener("click", loguear);
